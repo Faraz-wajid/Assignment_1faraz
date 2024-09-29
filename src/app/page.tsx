eec378e1e83c145ef6234e9a95bd40e38a4d834b
Faraz <faraz_wajid@yahoo.com>
2024-09-29 22:51:22 +0500

    Assignment 2

---
+++ src/app/page.tsx
@@ -0,0 +1,19 @@
+
+
+export default function Home() {
+  return (
+    <div className=" main">
+      <h1 id="Heading" >Assalam O Alikum!</h1> <br />
+      <p id="opening">My name is Faraz Wajid & I am Student at Governor Sindh Initiative for AI</p>
+      <img src="/faraz.jpg"/> <br />
+
+      <p className="list"> My Academic Particulars are as follows: </p>
+        <ul className="list">
+          <li>B.E Mechanical Engineering From NED University</li>
+          <li>M.E.M Energy & Plant Management from NED University</li>
+          <li>M.S Data Science from FAST University (in progress)</li>
+        </ul>
+    
+    </div>
+  );
+}
